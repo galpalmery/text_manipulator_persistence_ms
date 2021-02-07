f45392500f1bc27668de4a9cbbc7c611a306fbf2
galpalmery <galpalmery@gmail.com>
2021-02-07 02:36:27 +0200

    text-manipulator-persistence-ms

--- src/main/java/harel/interview/textmanipulator/controllers/TextManipulatorPersistenceController.java
+++ src/main/java/harel/interview/textmanipulator/controllers/TextManipulatorPersistenceController.java
@@ -1,10 +1,12 @@
 package harel.interview.textmanipulator.controllers;
 
+import harel.interview.textmanipulator.repository.model.TextManipulationDataModel;
 import harel.interview.textmanipulator.services.PersistToDBApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +15,20 @@ public class TextManipulatorPersistenceController {
     @Autowired
     private PersistToDBApplicationService persistToDBApplicationService;
 
-    @PostMapping("/textmanipulator/inputfilename/{inputfilename}/outputfilename/{outputfilename}/action/{action}")
-    public ResponseEntity<Object> runTextManipulationAction(@PathVariable("inputfilename") String inputFileName,
-                                                            @PathVariable("outputfilename") String outputFileName,
-                                                            @PathVariable("action") String action) {
-        return persistToDBApplicationService.execute(inputFileName, outputFileName, action);
+    //keeping this for testing purposes
+//    @PostMapping("/textmanipulator/inputfilename/{inputfilename}/outputfilename/{outputfilename}/action/{action}")
+//    public ResponseEntity<Object> persitToDBwithPathParams(@PathVariable("inputfilename") String inputFileName,
+//                                                           @PathVariable("outputfilename") String outputFileName,
+//                                                           @PathVariable("action") String action) {
+//        return persistToDBApplicationService.execute(inputFileName, outputFileName, action);
+//    }
+
+    //this is intercepted from feign client
+    @PostMapping("/textmanipulator")
+    public ResponseEntity<Object> persistToDB(@RequestBody TextManipulationDataModel textManipulationDataModel) {
+        System.out.println("inside persistToDB");
+
+        return persistToDBApplicationService.execute(textManipulationDataModel.getInputFileName(),
+                textManipulationDataModel.getOutputFileName(), textManipulationDataModel.getAction());
     }
 }
